6e273aa877ca46353319842344068b7d0545ea18
SHOGO180 <s.uenaka@180-inc.com>
2024-01-18 06:20:02 +0900

    finish Options API

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,64 @@
+<!-- <template> -->
+  <!-- <div>
+    <h2>親コンポーネント</h2>
+    <input type="text" v-model="parentData">
+    <the-child
+    :dataFromParent="parentData"
+    @child-event="receiveDataFromChild"
+    ></the-child>
+  </div> -->
+<!-- </template> -->
+
 <template>
+  <div>
+    <h2>親コンポーネント</h2>
+    <input v-model="parentData" type="text">
+    <the-child 
+    :dataFromParent="parentData"
+    @child-event="receiveDataFromChild"
+    ></the-child>
+  </div>
 </template>
 
+<script>
+import TheChild from './components/TheChild.vue';
+
+export default {
+  components: {
+    TheChild,
+  },
+  data() {
+    return {
+      parentData: '',
+    };
+  },
+  methods: {
+    receiveDataFromChild(dataFromChild) {
+      this.parentData = dataFromChild;
+    },
+  },
+}
+</script>
+
 <!-- Option API -->
+<!-- <script>
+import TheChild from './components/TheChild.vue';
+
+export default {
+  components: {
+    TheChild,
+  },
+  data() {
+    return {
+      parentData: '',
+    };
+  },
+  methods: {
+    receiveDataFromChild(DataFromChild) {
+      this.parentData = DataFromChild;
+    },
+  },
+} -->
+<!-- </script> -->
 
 <!-- Composition API NEW -->
---
+++ src/components/TheChild.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h2>子コンポーネント</h2>
+    <p>親から受け取ったデータですよ：{{ dataFromParent }}</p>
+    <button @click="sendDataToParent">子のデータを親に送る</button>
+  </div>
+  <!-- <div>
+    <h2>子コンポーネント</h2>
+    <p>親から受け取ったデータ：{{ dataFromParent }}</p>
+    <button @click="sendDataToParent">親にデータを送る</button>
+  </div> -->
+</template>
+
+<script>
+export default {
+  props: ['dataFromParent'],
+  emits:['child-event'],
+  data() {
+    return {
+      childData: '子が持っているデータですよ',
+    };
+  },
+  methods: {
+    sendDataToParent() {
+      this.$emit('child-event', this.childData);
+    },
+  },
+}
+</script>
+
+<!-- Option API -->
+<!-- <script>
+export default {
+  props: ['dataFromParent'],
+  emits: ['child-event'],
+
+  data() {
+    return {
+      childData: '子が持つデータ',
+    };
+  },
+  methods: {
+    sendDataToParent() {
+      this.$emit('child-event', this.childData);
+    },
+  },
+}
+</script> -->
+
+<!-- Composition API NEW -->
+<!-- <script setup>
+import { ref, defineProps, defineEmits } from 'vue';
+
+const props = defineProps(['dataFromParent']);
+const emits = defineEmits(['child-event']);
+
+const childData = ref('子が持つデータ');
+
+const sendDataToParent = () => {
+  emits('child-event', childData.value);
+};
+
+</script> -->
+
